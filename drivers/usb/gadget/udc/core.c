f070045ce6d8aa7fb8b0621d3f160103a2f670d3
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-01-06 19:12:54 +0000

    remove buggy support for USB gadget mode from deny_new_usb2
    The removed change broke switching between host and gadget mode on 10th gen Pixels.
    
    This change was used as an extra layer of protection on 6th-9th gen Pixels. 10th gen Pixels
    include an extra layer of USB port protection via the disable_usb_data option in google-role-sw
    driver.

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -714,12 +714,6 @@ static int usb_gadget_connect_locked(struct usb_gadget *gadget)
 		goto out;
 	}
 
-	if (deny_new_usb) {
-		dev_err(&gadget->dev, "blocked USB gadget connection\n");
-		ret = -EPERM;
-		goto out;
-	}
-
 	if (gadget->deactivated || !gadget->udc->allow_connect || !gadget->udc->started) {
 		/*
 		 * If the gadget isn't usable (because it is deactivated,
