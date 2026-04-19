ac338947716c817cfb0ffd9252c0738c8e6a676a
Daniel Micay <daniel.micay@grapheneos.org>
2026-04-19 12:35:49 -0400

    Partially revert "ANDROID: defer zeroing to allocation context in init_on_free"
    This reverts commit 7e1ae40ab7521834e2c3513931dbe8e816acf711 with the
    hook definitions left intact for compatibility.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1319,11 +1319,8 @@ static __always_inline bool free_pages_prepare(struct page *page,
 		if (kasan_has_integrated_init())
 			init = false;
 	}
-	if (init) {
-		trace_android_vh_free_pages_prepare_init(page, 1 << order, &init);
-		if (init)
-			kernel_init_pages(page, 1 << order);
-	}
+	if (init)
+		kernel_init_pages(page, 1 << order);
 
 	/*
 	 * arch_free_page() can make the page's contents inaccessible.  s390
@@ -1611,11 +1608,8 @@ static void check_new_page_bad(struct page *page)
  */
 static int check_new_page(struct page *page)
 {
-	unsigned long check_flags = PAGE_FLAGS_CHECK_AT_PREP|__PG_HWPOISON;
-
-	trace_android_vh_check_new_page(&check_flags);
-
-	if (likely(page_expected_state(page, check_flags)))
+	if (likely(page_expected_state(page,
+				PAGE_FLAGS_CHECK_AT_PREP|__PG_HWPOISON)))
 		return 0;
 
 	check_new_page_bad(page);
@@ -1667,13 +1661,10 @@ static inline bool should_skip_init(gfp_t flags)
 inline void post_alloc_hook(struct page *page, unsigned int order,
 				gfp_t gfp_flags)
 {
-	int i;
-	bool zero_tags;
 	bool init = !want_init_on_free() && want_init_on_alloc(gfp_flags) &&
 			!should_skip_init(gfp_flags);
-
-	trace_android_vh_post_alloc_hook(page, order, &init);
-	zero_tags = init && (gfp_flags & __GFP_ZEROTAGS);
+	bool zero_tags = init && (gfp_flags & __GFP_ZEROTAGS);
+	int i;
 
 	set_page_private(page, 0);
 	set_page_refcounted(page);
