b35f35dd5b8c28329cdc500ed0da6e3c7d45d6b9
Sebastian Ene <sebastianene@google.com>
2026-07-02 13:53:03 +0000

    Revert "ANDROID: firmware: arm_ffa: Remove unused variable"
    This reverts commit 36455e99b8c4e9baf362e914ed79680435d6b155.
    
    Change-Id: I5bc522d1a9c992686b2c795adaa57b7e88c8639a
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- drivers/firmware/arm_ffa/driver.c
+++ drivers/firmware/arm_ffa/driver.c
@@ -457,7 +457,7 @@ ffa_setup_and_transmit(u32 func_id, void *buffer, u32 max_fragsize,
 	struct ffa_composite_mem_region *composite;
 	struct ffa_mem_region_addr_range *constituents;
 	struct ffa_mem_region_attributes *ep_mem_access;
-	u32 idx, frag_len, length, buf_sz = 0, num_entries = sg_nents(args->sg);
+	u32 idx, frag_len, length, buf_sz = 0, num_entries = sg_nents(args->sg), ep_offset;
 
 	mem_region->tag = args->tag;
 	mem_region->flags = args->flags;
